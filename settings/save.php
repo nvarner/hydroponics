7751d1bc2d8970a739586bc6e6c4f91c8e4f4c32
medude <medude@users.noreply.github.com>
2017-03-25 11:51:15 -0400

    Things done

--- settings/save.php
+++ settings/save.php
@@ -1,3 +1,11 @@
 <?php
-echo json_encode($_POST);
+require_once("../functions.php");
+
+$json_file = get_json("../data/settings.json");
+$json_update = $_POST;
+
+foreach ($json_update as $key => $value) {
+	$json_file[$key] = $value;
+	echo $json_file[$key];
+}
 ?>
