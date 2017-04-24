a6db2193d58f4a0bf1c220e186e39ea4ba333f68
medude <medude@users.noreply.github.com>
2017-04-24 17:02:28 -0400

    Change design, header
    Closer to Material Design specs, looks a lot better.

--- plants/info/index.php
+++ plants/info/index.php
@@ -20,41 +20,76 @@ $plant_json = get_json("../$plant_type.json");
 <body>
 	<?php include("../../templates/header.php"); ?>
 	<h1><?php echo $plant_type_nice ?></h1>
-
-	<h2>Description</h2>
 	<p><?php echo $plant_json->{"description"} ?></p>
 
-	<h2>Symptoms</h2>
-	<ul class="nice level-2">
-		<?php
+	<div class="card level-2 full-cell expand-panel">
+		<div class="expand-panel-top">
+			<p class="expand-panel-title">Symptoms</p>
+			<p class="expand-panel-description">The name and description of all
+				symptoms this plant may experience.</p>
+		</div>
+		<div class="expand-panel-content">
+			<table class="full-size">
+				<thead>
+					<tr>
+						<td>Name</td>
+						<td>Description</td>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
 
-		foreach ($plant_json->{"symptoms-list"} as $symptom_key => $symptom_value) {
-			echo "<li><span class='bold'>" . nice_text($symptom_key) . "</span>- $symptom_value</li>";
-		}
+					foreach ($plant_json->{"symptoms-list"} as$symptom_key => $symptom_value) {
+						echo "<tr>";
+						echo "<td>" . nice_text($symptom_key) . "</td><td>$symptom_value</td>";
+						echo "</tr>";
+					}
 
-		?>
-	</ul>
+					?>
+				</tbody>
+			</table>
+		</div>
+	</div>
 
-	<h2>Issues</h2>
-	<ul class="nice level-2">
-		<?php
+	<div class="card level-2 full-cell expand-panel">
+		<div class="expand-panel-top">
+			<p class="expand-panel-title">Issues</p>
+			<p class="expand-panel-description">All of the issues that a plant may experience.</p>
+		</div>
+		<div class="expand-panel-content">
+			<ul class="list">
+				<?php
 
-		foreach ($plant_json->{"issues-list"} as $issue) {
-			$issue_name = nice_text($issue[0]);
-			$issue_symptoms = $issue[1];
-			$issue_resources = $issue[2];
+				foreach ($plant_json->{"issues-list"} as $issue) {
+					$issue_unnice = $issue[0];
+					$issue_name = nice_text($issue[0]);
+					$issue_symptoms = $issue[1];
+					$issue_resources = $issue[2];
 
-			echo "<h3>$issue_name</h3>";
-			echo "<ul class='nice level-2'>";
-			foreach ($issue_symptoms as $symptom) {
-				echo "<li>" . nice_text($symptom) . "</li>";
-			}
-			echo "</ul>";
-		}
+					echo "<li>";
+					echo "<div class='card full-cell expand-panel'>";
 
-		?>
-	</ul>
+						echo "<div class='expand-panel-top'>";
+							echo "<p class='expand-panel-title'>$issue_name</p>";
+							echo "<p class='expand-panel-description'></p>";
+						echo "</div>";
 
+						echo "<div class='expand-panel-content'>";
+						echo "<ul>";
+						foreach ($issue_symptoms as $symptom) {
+							echo "<li>" . nice_text($symptom) . "</li>";
+						}
+						echo "</ul>";
+						echo "</div>";
+
+					echo "</div>";
+					echo "</li>";
+				}
+
+				?>
+			</ul>
+		</div>
+	</div>
 </body>
 
 <?php include("../../templates/foot.php"); ?>
