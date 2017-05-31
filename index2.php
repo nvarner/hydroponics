d42eb550e3125e5a9947b116827dcba7f4a6270d
medude <medude@users.noreply.github.com>
2017-05-30 20:00:17 -0400

    Remove SMS
    It didn't work, would require extra work to fix. Will attempt to solve
    at a later date, will create an issue for it.

--- index2.php
+++ index2.php
@@ -6,21 +6,8 @@ include("templates/head.php");
 ?>
 
 <body>
-	<?php include("templates/header.php"); ?>
-	<h1 class="mdc-typography--headline mdc-typography--adjust-margin">Koi Hydroponics System</h1>
-	<div class="photoalbum">
-		<div class="mdc-card">
-			<img class="mdc-card__media" src="" alt="tomato" />
-			<section class="mdc-card__primary">
-				<h1 class="mdc-card__title mdc-card__title--large">Tomato</h1>
-				<h2 class="mdc-card__subtitle"></h2>
-			</section>
-			<section class="mdc-card__actions">
-				<button class="mdc-button mdc-button--compact mdc-card__action">View</button>
-				<button class="mdc-button mdc-button--compact mdc-card__action">Delete</button>
-
-			</section>
-		</div>
-	</div>
+	<header class="md-toolbar">
+		<h1 class="mdc-typography--headline mdc-typography--adjust-margin">Koi Hydroponics System</h1>
+	</header>
 </body>
 <?php include("templates/foot.php"); ?>
